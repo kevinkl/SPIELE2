1e5cfa8c18695fa0f6213749511318ac8fc0fb0e
Daniel Scherzer <scherzer@hs-weingarten.de>
2016-04-04 13:24:18 +0200

    vbo particle system

--- Examples/ShaderVBOExample/Program.cs
+++ Examples/ShaderVBOExample/Program.cs
@@ -13,21 +13,23 @@ namespace Example
 	{
 		struct VertexFormat
 		{
-			Vector3 position;
-			Vector3 velocity;
+			public Vector2 position;
+			public Vector2 velocity;
 
-			public VertexFormat(Vector3 position, Vector3 velocity)
+			public VertexFormat(Vector2 position, Vector2 velocity)
 			{
 				this.position = position;
 				this.velocity = velocity;
 			}
 
 			public static readonly int size = Marshal.SizeOf(typeof(VertexFormat));
+			public static readonly int startVelocity = Marshal.SizeOf(typeof(Vector2));
 		};
 
 		private GameWindow gameWindow = new GameWindow();
-		private const int particelCount = 400;
+		private const int particelCount = 1000;
 		private Shader shader;
+		private TimeSource timeSource = new TimeSource(50.0f);
 
 		[STAThread]
 		public static void Main()
@@ -44,23 +46,27 @@ namespace Example
 		private MyApplication()
 		{
 			gameWindow.KeyDown += (s, arg) => { if (arg.Key == Key.Escape) gameWindow.Close(); };
+			gameWindow.Resize += (s, arg) => GL.Viewport(0, 0, gameWindow.Width, gameWindow.Height);
 			gameWindow.RenderFrame += game_RenderFrame;
 			LoadShader();
 			InitVBOs();
+			timeSource.IsLooping = true;
+			timeSource.IsRunning = true;
 		}
 
 		private void InitVBOs()
 		{
 			var rnd = new Random(12);
 			Func<float> RndCoord = () => (float)((rnd.NextDouble() - 0.5) * 2.0);
-			Func<float> RndSpeed = () => (float)((rnd.NextDouble() - 0.5) * 20.0);
+			Func<float> RndSpeed = () => (float)((rnd.NextDouble() - 0.5) * 0.1);
 			var vertices = new List<VertexFormat>();
-			for (uint i = 0; i < particelCount; ++i)
+			for (int i = 0; i < particelCount; ++i)
 			{
 				vertices.Add(new VertexFormat(
-					new Vector3(RndCoord(), RndCoord(), RndCoord()),
-					new Vector3(RndSpeed(), RndSpeed(), RndSpeed())
+					new Vector2(RndCoord(), RndCoord()),
+					new Vector2(RndSpeed(), RndSpeed())
 					));
+				Console.WriteLine(vertices[i].velocity);
 			}
 			uint vbo; //our vbo handler
 			GL.GenBuffers(1, out vbo);
@@ -72,10 +78,11 @@ namespace Example
 		{
 			string sVertexShader = @"
 				#version 430 core				
-				layout(location = 0) in vec3 in_position;  //set the frist input on location (index) 0 ; in_position is our attribute 
-				//layout(location = 1) in vec4 in_speed;
+				layout(location = 0) in vec2 in_position;  //set the frist input on location (index) 0 ; in_position is our attribute 
+				layout(location = 1) in vec2 in_speed;
+				uniform float time;
 				void main() {
-					gl_Position = vec4(in_position, 1.0);//w is 1.0, also notice cast to a vec4
+					gl_Position = vec4(in_position + time * in_speed, 0.0, 1.0);//w is 1.0, also notice cast to a vec4
 				}";
 			string sFragmentShd = @"
 			#version 430 core
@@ -102,10 +109,11 @@ namespace Example
 			GL.Clear(ClearBufferMask.ColorBufferBit);
 			GL.PointSize(5.0f);
 			shader.Begin();
+			GL.Uniform1(shader.GetUniformLocation("time"), timeSource.Position);
 			GL.EnableVertexAttribArray(0);
-			GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, VertexFormat.size, 0);
+			GL.VertexAttribPointer(0, 2, VertexAttribPointerType.Float, false, VertexFormat.size, 0);
 			GL.EnableVertexAttribArray(1);
-			GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, VertexFormat.size, 9);
+			GL.VertexAttribPointer(1, 2, VertexAttribPointerType.Float, false, VertexFormat.size, VertexFormat.startVelocity);
 			GL.DrawArrays(PrimitiveType.Points, 0, particelCount);
 			shader.End();
 			GL.DisableVertexAttribArray(1);
